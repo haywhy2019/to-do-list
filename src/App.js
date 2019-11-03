275b14c485f61c8a05dba26e59dca3e64015fc8a
flint <olusegun.sola19@gmail.com>
2019-11-03 17:49:46 +0100

    yes

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import Header from './components/layout/Header'; 
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+
+import './App.css';
+
+class App extends Component {
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id: 3 ,
+        title: 'meeting with boss',
+        completed: false
+      }
+    ]
+  }
+  //toggle complete
+  markComplete=(id)=>{
+   this.setState({todos: this.state.todos.map(todo =>{
+    if(todo.id === id) {
+      todo.completed = !todo.completed
+    }
+    return todo;
+   }) });
+  }
+
+  // delete todo
+  delTodo = (id) =>{
+  this.setState( { todos: [...this.state.todos.filter(todo => todo.id
+    !== id)] })
+  }
+  
+
+  //addTodotitle
+  AddTodo= (title) =>{
+    const newTodo =  {
+      id: 4,
+      title: title,
+      completed: false
+    }
+    this.setState( {todos: [...this.state.todos, newTodo] })
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <div className="container">
+          <Header/>
+          <AddTodo AddTodo={this.AddTodo}/>
+          <Todos todos={this.state.todos} markComplete=
+          {this.markComplete} delTodo={this.delTodo}/>
+        </div>  
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
