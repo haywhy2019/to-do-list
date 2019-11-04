cc79df7c79bf418c178997edb1dbd902a2b4c4ab
flint <olusegun.sola19@gmail.com>
2019-11-04 03:38:09 +0100

    Updates

---
+++ precache-manifest.60da4f515f5da47456b2a4a524ead5a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2362113ec3819da4f10976724256557e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3dcc91d52a71d270386",
+    "url": "/static/css/main.51ad9f50.chunk.css"
+  },
+  {
+    "revision": "cd94703d74485988b9b9",
+    "url": "/static/js/2.0d4892c0.chunk.js"
+  },
+  {
+    "revision": "a3dcc91d52a71d270386",
+    "url": "/static/js/main.88e7964e.chunk.js"
+  },
+  {
+    "revision": "2eb5f0810f3d957883a9",
+    "url": "/static/js/runtime-main.f99ad821.js"
+  }
+]);
