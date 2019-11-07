794a153acb8bd722f4ddb2393fff9337ec6eacb0
flint <olusegun.sola19@gmail.com>
2019-11-07 10:37:41 +0100

    Updates

---
+++ precache-manifest.9350e70fc027fc42432ac6ee31e9fdba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f6da5365a765bc0ab41435d2d272936",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "721200149b42ebbbae93",
+    "url": "/to-do-list/static/css/main.51ad9f50.chunk.css"
+  },
+  {
+    "revision": "8e8ed34e1fe93edbd78e",
+    "url": "/to-do-list/static/js/2.e883ea94.chunk.js"
+  },
+  {
+    "revision": "721200149b42ebbbae93",
+    "url": "/to-do-list/static/js/main.dc3c8414.chunk.js"
+  },
+  {
+    "revision": "038f86d6469f081bf28c",
+    "url": "/to-do-list/static/js/runtime-main.ea8822b0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60da4f515f5da47456b2a4a524ead5a8.js"
+  "/to-do-list/precache-manifest.9350e70fc027fc42432ac6ee31e9fdba.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/to-do-list/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
